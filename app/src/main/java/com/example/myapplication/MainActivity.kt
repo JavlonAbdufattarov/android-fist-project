653db1b05036fbfd6143856e54871cdcf8818ed0
Javlon <abdufattarovjavlon@gmail.com>
2024-05-01 12:58:58 +0300

    исправил все не достатки

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -24,18 +25,26 @@ class MainActivity : AppCompatActivity() {
         biding.textView5.text=counter.toString()
         biding.textView5.text = getText(R.string.testText)
         biding.sw.setOnClickListener{
-            counter++
+
+            if(MAX_VALUES>counter){
+                counter++
             printValue = 50 - counter
-            if (printValue<0){
-                counter=0
-                printValue =0
-                biding.textView5.text = "Пасажиров слижком много!"
+            }
+            if (printValue==0){
+                biding.textView5.apply {
+                    text = "Пассажиров слишком много!"
+                    setTextColor(resources.getColor(android.R.color.holo_red_dark))
+
+                }
 
             }
             else {
-                biding.textView5.text = "Осталость мест: " + printValue.toString()
+                biding.textView5.apply { text = "Осталость мест: " + printValue.toString()
+                    setTextColor(resources.getColor(android.R.color.holo_blue_dark))  }
             }
+
         }
+
         biding.mines.setOnClickListener {
             if(counter == 0){
                 counter = 0
@@ -43,11 +52,26 @@ class MainActivity : AppCompatActivity() {
             else{
                 counter--
                 printValue = MAX_VALUES - counter
-            biding.textView5.text = "Осталость мест: "+ printValue.toString()
-        }}
+            biding.textView5.apply { text = "Осталость мест: " + printValue.toString()
+                setTextColor(resources.getColor(android.R.color.holo_blue_dark))  }
+            }
+        }
 
         biding.bottom.setOnClickListener{
-            biding.textView5.text = "bottom is clicked!"
+            counter = 0
+            printValue = 0
+            biding.textView5.apply { text = "Все места свободны"
+                setTextColor(resources.getColor(android.R.color.holo_green_dark))  }
+
+
+        }
+
+         fun updateButtonVisibility(binding: ActivityMainBinding) {
+            if (counter == MAX_VALUES) {
+                binding.bottom.visibility = View.VISIBLE
+            } else {
+                binding.bottom.visibility = View.GONE
+            }
         }
 
 
